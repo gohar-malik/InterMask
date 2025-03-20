153ca62c99ef37b7ead0571580a0dcfb9c6572f1
Gohar <goharjavid@gmail.com>
2025-03-19 22:15:11 -0600

    update eval download

--- prepare/download_evaluator.sh
+++ prepare/download_evaluator.sh
@@ -4,4 +4,5 @@ echo "The pretrained evaluation model will be stored in the './checkpoints/eval_
 # InterHuman
 echo "Downloading the evaluation model..."
 gdown https://drive.google.com/uc?id=1bJv5lTP7otJleaBYZ2byjru_k_wCsGvH
+gdown https://drive.google.com/uc?id=13NzdH-9xSOhrOCEVY8lmYrSzebZNv5xz
 echo "Evaluation model downloaded"
