60eb48588fe273be6e2565a103afe6ce7282a786
Gohar <goharjavid@gmail.com>
2025-04-23 19:26:05 -0600

    update model ckpt

--- prepare/download_models.py
+++ prepare/download_models.py
@@ -1,5 +1,5 @@
 import gdown
 
-ckpt_folder_url = 'https://drive.google.com/drive/folders/1vhZ8cbSHvJf5IBg2I_y2PNZA1_DhlpK8'
-ckpt_folder_id = '1vhZ8cbSHvJf5IBg2I_y2PNZA1_DhlpK8'
+ckpt_folder_url = 'https://drive.google.com/drive/folders/1WCFR7Opc5S3cke26cjEhdvSOH_CXL2Ut'
+ckpt_folder_id = '1WCFR7Opc5S3cke26cjEhdvSOH_CXL2Ut'
 gdown.download_folder(id = ckpt_folder_id)
