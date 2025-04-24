968031102785d80fbf3136a1a90259c17903d63f
Gohar <goharjavid@gmail.com>
2025-04-23 19:11:55 -0600

    fix repro issue

--- models/vq/encdec.py
+++ models/vq/encdec.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch_geometric.nn.conv import SAGEConv
+
 from models.vq.resnet import Resnet
 from utils.paramUtil import t2m_edge_indices as edge_indices
 
@@ -18,7 +20,8 @@ class Encoder(nn.Module):
                  activation='relu',
                  norm=None,
                  filter_s=None,
-                 stride_s=None):
+                 stride_s=None,
+                 gcn=False):
         super().__init__()
 
         conv_layer = nn.Conv2d
@@ -28,8 +31,15 @@ class Encoder(nn.Module):
         filter_s = filter_t if filter_s is None else filter_s
         stride_s = stride_t if stride_s is None else stride_s
 
-        blocks.append(conv_layer(input_emb_width, width, 3, 1, 1))
-        blocks.append(nn.ReLU())
+        self.gcn = gcn
+        if self.gcn:
+            self.gcn_layer1 = SAGEConv(input_emb_width, width, project=True)
+            self.gcn_act1 = nn.ReLU()
+            self.gcn_layer2 = SAGEConv(width, width, project=True)
+            self.gcn_act2 = nn.ReLU()
+        else:
+            blocks.append(conv_layer(input_emb_width, width, 3, 1, 1))
+            blocks.append(nn.ReLU())
 
         for i in range(down_t):
             input_dim = width
@@ -41,8 +51,23 @@ class Encoder(nn.Module):
         blocks.append(conv_layer(width, output_emb_width, 3, 1, 1))
         self.model = nn.Sequential(*blocks)
 
+    def gcn_forward(self, x, edge_indices=edge_indices):
+        B, D, J, T = x.shape
+        x = x.permute(0,3,2,1)
+        x = x.reshape(-1, x.shape[2], x.shape[3])
+        x = x.reshape(-1, x.shape[2])
+        edge_indices = edge_indices.to(x.device)
+        x = self.gcn_act1(self.gcn_layer1(x, edge_indices))
+        x = self.gcn_act2(self.gcn_layer2(x, edge_indices))
+        x = x.reshape(-1, J, x.shape[1])
+        x = x.reshape(B, -1, x.shape[1], x.shape[2])
+        x = x.permute(0,3,2,1)
+        return x
+    
     def forward(self, x):
-
+        if self.gcn:
+            x = self.gcn_forward(x)
+        
         return self.model(x)
 
 
@@ -58,7 +83,8 @@ class Decoder(nn.Module):
                  dilation_growth_rate=3,
                  activation='relu',
                  norm=None,
-                 spatial_upsample=None):
+                 spatial_upsample=None,
+                 gcn=False):
         super().__init__()
         
         conv_layer = nn.Conv2d
@@ -81,16 +107,40 @@ class Decoder(nn.Module):
                 conv_layer(width, out_dim, 3, 1, 1)
             )
             blocks.append(resnet)
-        
+
         blocks.append(conv_layer(width, width, 3, 1, 1))
         blocks.append(nn.ReLU())
-        blocks.append(conv_layer(width, input_emb_width, 3, 1, 1))
-        
+
+        self.gcn = gcn
+        if not self.gcn:
+            blocks.append(conv_layer(width, input_emb_width, 3, 1, 1))
+        else:
+            self.gcn_layer1 = SAGEConv(width, width, project=True)
+            self.gcn_act = nn.ReLU()
+            self.gcn_layer2 = SAGEConv(width, input_emb_width, project=True)
+
         self.model = nn.Sequential(*blocks)
+            
            
+    def gcn_forward(self, x, edge_indices=edge_indices):
+        B, D, J, T = x.shape
+        x = x.permute(0,3,2,1)
+        x = x.reshape(-1, x.shape[2], x.shape[3])
+        x = x.reshape(-1, x.shape[2])
+        edge_indices = edge_indices.to(x.device)
+        x = self.gcn_layer1(x, edge_indices)
+        x = self.gcn_layer2(self.gcn_act(x), edge_indices)
+        x = x.reshape(-1, J, x.shape[1])
+        x = x.reshape(B, -1, x.shape[1], x.shape[2])
+        x = x.permute(0,3,2,1)
         
+        return x
+    
     def forward(self, x):
         x = self.model(x)
+        
+        if self.gcn:
+            x = self.gcn_forward(x)
         
         return x
     
